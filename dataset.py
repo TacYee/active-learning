a7624df5b2bb928fe3c946f57e914460957c013d
TacYee <72900874+TacYee@users.noreply.github.com>
2022-02-10 13:18:13 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,44 @@
+'''
+@author:Chaoxiang  Ye 
+    11.05.2021
+
+'''
+
+import numpy as np
+import torch
+from torchvision import datasets
+from torch.utils.data import Dataset
+from PIL import Image
+
+def get_dataset(name):
+    if name == 'MNIST':
+        return get_MNIST()
+
+def get_MNIST():
+    raw_tr = datasets.MNIST('./MNIST', train=True, download=True)
+    raw_te = datasets.MNIST('./MNIST', train=False, download=True)
+    X_tr = raw_tr.train_data
+    Y_tr = raw_tr.train_labels
+    X_te = raw_te.test_data
+    Y_te = raw_te.test_labels
+    return X_tr, Y_tr, X_te, Y_te
+
+def get_handler(name):
+    if name == 'MNIST':
+        return DataHandler
+
+class DataHandler(Dataset):
+    def __init__(self, X, Y, transform=None):
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.X[index], self.Y[index]
+        if self.transform is not None:
+            x = Image.fromarray(x.numpy(), mode='L')
+            x = self.transform(x)
+        return x, y, index
+
+    def __len__(self):
+        return len(self.X)
---
+++ method.py
@@ -0,0 +1,39 @@
+'''
+@author:Chaoxiang  Ye 
+    11.05.2021
+
+'''
+
+import numpy as np
+import torch
+from strategy import Strategy
+
+class RandomSampling(Strategy):
+	def __init__(self, X, Y, idxs_lb, net, handler, args):
+		super(RandomSampling, self).__init__(X, Y, idxs_lb, net, handler, args)
+
+	def query(self, n):
+		return np.random.choice(np.where(self.idxs_lb==0)[0], n)
+		
+class MarginSampling(Strategy):
+	def __init__(self, X, Y, idxs_lb, net, handler, args):
+		super(MarginSampling, self).__init__(X, Y, idxs_lb, net, handler, args)
+
+	def query(self, n):
+		idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
+		probs = self.predict_prob(self.X[idxs_unlabeled], self.Y[idxs_unlabeled])
+		probs_sorted, idxs = probs.sort(descending=True)
+		U = probs_sorted[:, 0] - probs_sorted[:,1]
+		return idxs_unlabeled[U.sort()[1][:n]]
+
+class MarginSamplingDropout(Strategy):
+	def __init__(self, X, Y, idxs_lb, net, handler, args, n_drop=10):
+		super(MarginSamplingDropout, self).__init__(X, Y, idxs_lb, net, handler, args)
+		self.n_drop = n_drop
+
+	def query(self, n):
+		idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
+		probs = self.predict_prob_dropout(self.X[idxs_unlabeled], self.Y[idxs_unlabeled], self.n_drop)
+		probs_sorted, idxs = probs.sort(descending=True)
+		U = probs_sorted[:, 0] - probs_sorted[:,1]
+		return idxs_unlabeled[U.sort()[1][:n]]
